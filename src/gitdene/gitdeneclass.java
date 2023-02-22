ccfd191be9abf0dc6d221e8a08e11c46aed77bd7
alioakay <alioakay@gmail.com>
2023-02-22 20:27:56 +0300

    dfj

--- src/gitdene/gitdeneclass.java
+++ src/gitdene/gitdeneclass.java
@@ -3,5 +3,6 @@ package gitdene;
 public class gitdeneclass {
     public static void main(String[] args) {
         System.out.println("kendim deniyom");
+        System.out.println("jdfjfgit");
     }
 }
